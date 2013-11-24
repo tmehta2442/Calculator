c41ba7491e514d05d709e46d9776c68d0e4e2bb0
tmehta2442 <tejas@bootinguptj.com>
2013-11-23 16:07:11 -0800

    rails calculator

---
+++ calculator/calculator/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calculator::Application.config.secret_token = '3bc9262b576549e96ceb08f699c0629cd19da00d711219e4b8301b86b5728236f69a43247aba72005bfaee4f3d78894c73054b3b10ced13ab23754afaef960ed'
